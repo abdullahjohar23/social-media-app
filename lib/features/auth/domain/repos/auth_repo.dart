fe4da92473192451f0de64d80e85ca468083e48a
Abdullah Johar <jrrabdullah23@gmail.com>
2026-04-30 20:17:59 +0600

    auth repo file created

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,14 @@
+/*
+
+Auth Repository - Outlines the possible auth operations for this app
+
+*/
+
+import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
+
+abstract class AuthRepo {
+    Future<AppUser?> loginWithEmailAndPassword(String email, String password);
+    Future<AppUser?> registerWithEmailAndPassword(String email, String password, String name);
+    Future<void> logout();
+    Future<AppUser?> getCurrentUser();
+}
