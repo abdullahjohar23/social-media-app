3169193c1b885482d9d6ff649006e21b051f1798
Abdullah Johar <jrrabdullah23@gmail.com>
2026-05-01 16:07:20 +0600

    code update for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:social_media_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+
     runApp(const MyApp());
 }
 
